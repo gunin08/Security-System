1fea3f206cff4c8083fab8aefabd0125fe2ff05f
gunin08 <153124598+gunin08@users.noreply.github.com>
2023-12-07 22:00:26 -0500

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,153 @@
+Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 1
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Security Alert! Employee is sleepy or intoxicated. Access denied.
+>>> 
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 0
+Does the person have a mustache? (1 for Yes, 0 for No): 1
+Does the person wear spectacles? (1 for Yes, 0 for No): 0
+Enter the skin colour of the person: fair 
+Enter the eye colour of the person: blue
+Enter the hair colour of the person: black
+Employee details do not match. Access Denied.
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 0
+Is there only one person in the frame? (1 for Yes, 0 for No): fair
+Invalid input. Please enter a valid Employee ID.
+Enter Employee ID: blue
+Invalid input. Please enter a valid Employee ID.
+Enter Employee ID: black
+Invalid input. Please enter a valid Employee ID.
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 0
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Access Denied. Conditions not met. Please try again.
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 0
+Does the person have a mustache? (1 for Yes, 0 for No): 0
+Does the person wear spectacles? (1 for Yes, 0 for No): 0
+Enter the skin colour of the person: fair
+Enter the eye colour of the person: blue
+Enter the hair colour of the person: blonde
+Employee details do not match. Access Denied.
+Security Alert! Too many incorrect attempts. Access denied.
+
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 0
+Does the person have a mustache? (1 for Yes, 0 for No): 1
+Does the person wear spectacles? (1 for Yes, 0 for No): 0
+Enter the skin colour of the person: fair
+Enter the eye colour of the person: blue
+Enter the hair colour of the person: blonde
+Employee details match. Access Granted!
+
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 2
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 1
+Does the person have a mustache? (1 for Yes, 0 for No): 0
+Does the person wear spectacles? (1 for Yes, 0 for No): 1
+Enter the skin colour of the person: tan
+Enter the eye colour of the person: brown
+Enter the hair colour of the person: brown
+Employee details match. Access Granted!
+Do you want to add a new employee? (yes/no): yes
+
+Adding a new employee:
+Enter the new Employee ID: 101
+Does the new employee have a beard? (1 for Yes, 0 for No): 1
+Does the new employee have a mustache? (1 for Yes, 0 for No): 1
+Does the new employee wear spectacles? (1 for Yes, 0 for No): 1
+Enter the skin colour of the new employee: tan
+Enter the eye colour of the new employee: blue
+Enter the hair colour of the new employee: pink
+Enter authorization level for the new employee (1 or 2): 1
+New employee added successfully!
+
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 1
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 1
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Security Alert! Employee is sleepy or intoxicated. Access denied.
+
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 3
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 0
+Does the person have a mustache? (1 for Yes, 0 for No): 0
+Does the person wear spectacles? (1 for Yes, 0 for No): 0
+Enter the skin colour of the person: dark
+Enter the eye colour of the person: green
+Enter the hair colour of the person: black
+Employee details match. Access Granted!
+
+====== RESTART: /Users/guninsingh/Downloads/Studies/204_faceRecProject.py ======
+Enter Employee ID: 27
+Is the person sleepy? (1 for Yes, 0 for No): 0
+Is the person intoxicated? (1 for Yes, 0 for No): 0
+Is there sufficient light? (1 for Yes, 0 for No): 1
+Is there only one person in the frame? (1 for Yes, 0 for No): 1
+Is the face clear? (1 for Yes, 0 for No): 1
+Is the face looking in front? (1 for Yes, 0 for No): 1
+Initial Access Granted!
+Does the person have a beard? (1 for Yes, 0 for No): 0
+Does the person have a mustache? (1 for Yes, 0 for No): 0
+Does the person wear spectacles? (1 for Yes, 0 for No): 1
+Enter the skin colour of the person: dark
+Enter the eye colour of the person: brown
+Enter the hair colour of the person: black
+Employee details match. Access Granted!
